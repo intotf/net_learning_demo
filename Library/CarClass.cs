cde9474d137e2f75154efe8d17398b2b9b2847bf
intotf <intotf@163.com>
2017-09-17 01:25:10 +0800

    class CarList : IEnumerable<CarList>

--- Library/CarClass.cs
+++ Library/CarClass.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Model;
+using System.Collections;
 
 namespace Library
 {
@@ -19,4 +20,48 @@ namespace Library
             Console.WriteLine("正在生产德国车 [{0}]   颜色{1}   ID：{2}", t.Brand, t.Color, t.Id);
         }
     }
+
+    public class CarList : IEnumerable<CarList>
+    {
+        public CarList[] carList { get; set; }
+
+        public string name { get; set; }
+
+        public CarList(string name)
+        {
+            this.name = name;
+        }
+
+        public IEnumerable<CarList> GetEnumerableByName()
+        {
+            if (string.IsNullOrEmpty(this.name))
+            {
+                yield return new CarList("没有名字");
+                yield return new CarList("不知道什么意思");
+            }
+            else
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    yield return new CarList(i.ToString() + this.name);
+                }
+            }
+
+        }
+
+        public IEnumerable<CarList> GetEnumerable()
+        {
+            foreach (var item in carList)
+            {
+                yield return item;
+            }
+        }
+
+        IEnumerable System.Collections.IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerable();
+        }
+    }
+
+
 }
